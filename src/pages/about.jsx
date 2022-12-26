02a49f1584c79783349e3f76d8c8828922abaa33
Alihaji15 <NV21067@nvtc.edu.bh>
2022-12-26 12:08:02 +0300

    update portraite

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -10,7 +10,7 @@ import {
   GitHubIcon,
   LinkedInIcon,
 } from '@/components/SocialIcons'
-import portraitImage from '@/images/portrait.jpg'
+import portraitImageAli from '@/images/ali-profile.png'
 
 function SocialLink({ className, href, children, icon: Icon }) {
   return (
@@ -52,7 +52,7 @@ export default function About() {
           <div className="lg:pl-20">
             <div className="max-w-xs px-2.5 lg:max-w-none">
               <Image
-                src={portraitImage}
+                src={portraitImageAli}
                 alt=""
                 sizes="(min-width: 1024px) 32rem, 20rem"
                 className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
